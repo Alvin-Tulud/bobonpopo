efdca20f0a1f123271b2417c71ce69bc441315cf
LePuncake321 <lucky713ph@yahoo.com>
2024-10-28 00:09:01 -0700

    Removed object stuff and added layermask

--- bobonpopo/Assets/Scirpt/player/GridMovement.cs
+++ bobonpopo/Assets/Scirpt/player/GridMovement.cs
@@ -16,8 +16,6 @@ public class GridMovement : MonoBehaviour
     private Vector3 playerInitPos;
     private Vector3 playerEndPos;
     private GameObject ObjtoMove;
-    private Vector3 objInitPos;
-    private Vector3 objEndPos;
 
     // Start is called before the first frame update
     void Start()
@@ -71,28 +69,16 @@ public class GridMovement : MonoBehaviour
             {
                 transform.position = Vector3.Lerp(playerInitPos, playerEndPos, moveTimer / moveMaxTimer);
 
-                if (ObjtoMove != null)
-                {
-                    ObjtoMove.transform.position = Vector3.Lerp(objInitPos, objEndPos, moveTimer / moveMaxTimer);
-                }
-
                 moveTimer++;
             }
             else
             {
                 transform.position = worldGrid.LocalToCell(playerEndPos);
 
-                if (ObjtoMove != null)
-                {
-                    ObjtoMove.transform.position = worldGrid.LocalToCell(objEndPos);
-                }
-
                 moveTimer = 0;
 
                 playerInitPos = Vector3.zero;
                 playerEndPos = Vector3.zero;
-                objInitPos = Vector3.zero;
-                objEndPos = Vector3.zero;
 
                 ObjtoMove = null;
 
@@ -113,37 +99,35 @@ public class GridMovement : MonoBehaviour
         //enable move disable input
     //else not a valid place to move so do nothing
     private void directionChecker(Vector2 direction)
+         
     {
+        LayerMask interactNWalls = LayerMask.GetMask("interactable", "wall");
         RaycastHit2D hit;
-        hit = Physics2D.CircleCast(transform.position, 0.1f, direction, 1f);
+        hit = Physics2D.CircleCast(transform.position, 0.1f, direction, 1f, interactNWalls);
 
-        if (!hit)
+        if (hit)
         {
-            Debug.Log("Nothing");
-        }
-
-        // Do nothing if player will hit a wall
-        if (hit.transform.gameObject.layer == 8)
-        {
-            return;
-        }
-
-        // See if an interactable can be moved if player hits one
-        if (hit.transform.gameObject.layer == 7)
-        {
-            ObjtoMove = hit.transform.gameObject;
-            hit = Physics2D.CircleCast(transform.position, 0.1f, direction, 2f, 8);
-
-            // If object is immovable so is the player
-            if (hit.transform.gameObject.layer == 8 || hit.transform.gameObject.layer == 7)
+            // Do nothing if player will hit a wall
+            if (hit.transform.gameObject.layer == 8)
             {
-                Debug.Log("Immovable box");
                 return;
             }
-            Debug.Log("Should move box");
-            objInitPos = worldGrid.LocalToCell(hit.transform.position);
-            objEndPos = worldGrid.LocalToCell(hit.transform.position + (Vector3)direction);
+
+            // See if an interactable can be moved if player hits one
+            if (hit.transform.gameObject.layer == 7)
+            {
+                ObjtoMove = hit.transform.gameObject;
+                hit = Physics2D.CircleCast(transform.position, 0.1f, direction, 2f, 8);
+
+                // Logic for signalling the interactable to do stuff, ...
+                // Then determine if player moves or not here
+                Debug.Log("Interactable");
+            }
         }
+
+        Debug.Log("Nothing");
+
+        
 
         this.direction = direction;
         playerInitPos = worldGrid.LocalToCell(transform.position);
